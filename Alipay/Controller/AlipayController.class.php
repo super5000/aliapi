31b3b54e39c9b0b6b95e612aecafb6180d5755a5
supercz <supercaozhi@163.com>
2017-09-07 09:43:30 +0800

    1、接受支付宝同步跳转通知

--- Alipay/Controller/AlipayController.class.php
+++ Alipay/Controller/AlipayController.class.php
@@ -15,4 +15,31 @@ class AlipayController extends Controller {
     alipay($pay_data);
   }
 
+  // 支付宝支付完成返回状态判断(同步通知)
+  public function ali_return(){
+    $order_no = $_GET['out_trade_no'];
+    $trade_no = $_GET['trade_no'];
+    $total_fee = $_GET['total_fee'];
+    $is_success = $_GET['is_success'];
+    $trade_status = $_GET['trade_status'];
+    // 判断支付是否成功
+    if ($is_success == 'T' && $order_no == $_SESSION['order_no']) {
+      if($_GET['trade_status'] == 'TRADE_FINISHED' || $_GET['trade_status'] == 'TRADE_SUCCESS') {
+
+        // 确认支付成功前
+        // 1、验证订单金额是否一致
+        // 2、判断订单是否已做支付成功处理
+        // 支付成功后的业务逻辑
+        // 1、将订单更新为已支付
+        // 2、根据自己具体业务是否将订单中商品加入用户已购
+
+      }else {
+        $this->error('支付失败');
+      }
+    }else {
+      $this->error('支付失败');
+    }
+  }
+
+
 }
