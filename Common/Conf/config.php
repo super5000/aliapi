c15639ec703cd22987cdc5653ff5fdc2eb010421
supercz <supercaozhi@163.com>
2017-09-08 09:22:08 +0800

    1、修改配置文件

--- Common/Conf/config.php
+++ Common/Conf/config.php
@@ -8,7 +8,7 @@ return array(
   'DB_USER'               =>  '',      // 用户名
   'DB_PWD'                =>  '',  // 密码
   'DB_PORT'               =>  '',        // 端口
-  'DB_PREFIX'             =>  'class_',    // 数据库表前缀
+  'DB_PREFIX'             =>  '',    // 数据库表前缀
   'DB_PARAMS'             =>  array(), // 数据库连接参数
   'DB_DEBUG'  			      =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
   'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
